4f5e89f688af2fc53b6beaf5a7df126d10b2f454
Mic <tt402@outlook.com>
2025-02-15 15:54:05 +0800

    移除Entry中无效的SerialName注解。

--- src/main/kotlin/cn/cutemic/bot/model/context/ContextEntry.kt
+++ src/main/kotlin/cn/cutemic/bot/model/context/ContextEntry.kt
@@ -1,15 +1,10 @@
 package cn.cutemic.bot.model.context
 
-import kotlinx.serialization.SerialName
 
 data class ContextEntry(
-    @SerialName("keywords")
     val keywords: String,
-    @SerialName("answers")
     val answers: MutableList<AnswerEntry>,
-    @SerialName("count")
     var count: Int,
-    @SerialName("last_updated")
     var lastUpdated: Long
 ) {
     constructor(keywords: String) : this(
