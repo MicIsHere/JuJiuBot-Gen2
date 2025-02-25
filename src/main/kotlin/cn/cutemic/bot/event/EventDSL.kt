fd1dd22dfcb632694a25446e07761683b7dd4fc3
Mic <tt402@outlook.com>
2025-02-26 00:41:14 +0800

    feat：增加统一错误处理，将阻断事件传递单独抽离为函数。

--- src/main/kotlin/cn/cutemic/bot/event/EventDSL.kt
+++ src/main/kotlin/cn/cutemic/bot/event/EventDSL.kt
@@ -1,9 +1,10 @@
 package cn.cutemic.bot.event
 
-import cn.cutemic.bot.Bot
 import cn.cutemic.bot.database.GroupService
+import kotlinx.coroutines.runBlocking
 import love.forte.simbot.application.Application
 import love.forte.simbot.application.listeners
+import love.forte.simbot.component.onebot.v11.core.event.message.OneBotFriendMessageEvent
 import love.forte.simbot.component.onebot.v11.core.event.message.OneBotNormalGroupMessageEvent
 import love.forte.simbot.event.Event
 import love.forte.simbot.event.EventResult
@@ -16,25 +17,20 @@ class EventDSL {
     val groupService by inject<GroupService>(GroupService::class.java)
 
     inline fun <reified T : Event> on(
-        crossinline handler: suspend T.() -> EventResult
+        crossinline handler: suspend T.() -> EventResult,
     ) {
         listeners.add {
             listeners {
                 listen<T> { event ->
-                    when (event) {
-                        is OneBotNormalGroupMessageEvent -> {
-                            val messageEvent = event as OneBotNormalGroupMessageEvent
-                            if (groupService.read(messageEvent.groupId.toLong())!!.soberUpTime != null) { // 醉酒检查，阻断事件传递
-                                return@listen EventResult.empty()
-                            }
-
-                            if (groupService.read(messageEvent.groupId.toLong())!!.blocked != null) { // 群封禁检查，阻断事件传递
-                                return@listen EventResult.empty()
-                            }
+                    runCatching {
+                        if (processEvent(event) != EventResult.invalid()) {
+                            event.handler()
                         }
+                    }.onFailure {
+                        processError(event, it)
                     }
 
-                    event.handler()
+                    EventResult.empty()
                 }
             }
         }
@@ -42,5 +38,52 @@ class EventDSL {
 
     fun register(): Application.() -> Unit = {
         listeners.forEach { it() }
+    }
+
+    fun processError(event: Event, throwable: Throwable): EventResult{
+        val stackTrace = StringBuilder()
+
+        when (event) {
+            is OneBotNormalGroupMessageEvent -> {
+                throwable.stackTrace.forEach {
+                    stackTrace.append("在 ${it.className}.${it.methodName}:${it.lineNumber}\n")
+                }
+                runBlocking {
+                    event.reply("操作出现错误 (${throwable.message})：$throwable\n${stackTrace}")
+                }
+                throwable.printStackTrace()
+            }
+
+            is OneBotFriendMessageEvent -> {
+                throwable.stackTrace.forEach {
+                    stackTrace.append("在 ${it.className}.${it.methodName}:${it.lineNumber}\n")
+                }
+                runBlocking {
+                    event.reply("操作出现错误 (${throwable.message})：$throwable\n${stackTrace}")
+                }
+                throwable.printStackTrace()
+            }
+        }
+
+        stackTrace.clear()
+        return EventResult.invalid()
+    }
+
+    fun processEvent(event: Event): EventResult{
+        when (event) {
+            is OneBotNormalGroupMessageEvent -> {
+                runBlocking {
+                    val group = groupService.read(event.groupId.toLong())!!
+                    group.soberUpTime?.let { // 醉酒检查，阻断事件传递
+                        return@runBlocking EventResult.invalid()
+                    }
+
+                    group.blocked?.let { // 群封禁检查，阻断事件传递
+                        return@runBlocking EventResult.invalid()
+                    }
+                }
+            }
+        }
+        return EventResult.empty()
     }
 }
