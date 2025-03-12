abeb1256cf7b64a5ca72e11d1c5ac54af7cca6fc
Mic <tt402@outlook.com>
2025-03-12 22:16:10 +0800

    refactor: 牛牛人品提示信息

--- src/main/kotlin/run/mic/bot/module/impl/LuckValue.kt
+++ src/main/kotlin/run/mic/bot/module/impl/LuckValue.kt
@@ -1,9 +1,9 @@
-package cn.cutemic.bot.module.impl
+package run.mic.bot.module.impl
 
-import cn.cutemic.bot.module.BotModule
-import cn.cutemic.bot.util.IgnoreCommand
 import love.forte.simbot.component.onebot.v11.core.event.message.OneBotNormalGroupMessageEvent
 import love.forte.simbot.event.EventResult
+import run.mic.bot.module.BotModule
+import run.mic.bot.util.IgnoreCommand
 import java.time.LocalDate
 import kotlin.math.abs
 import kotlin.math.roundToInt
@@ -20,7 +20,7 @@ object LuckValue: BotModule("人品", "查看自己的人品值") {
                     return@on EventResult.empty()
                 }
                 val value = getValue(userId.toLong())
-                reply("博士，你今天的人品值是${getMessage(value)}")
+                reply(getMessage(value))
                 EventResult.empty()
             }
         }
@@ -37,18 +37,13 @@ object LuckValue: BotModule("人品", "查看自己的人品值") {
 
     private fun getMessage(code: Int): String {
         return when(code) {
-            100 -> "$code！$code！$code！！！！！"
-            99 -> "$code！但不是 100……"
-            in 90..100 -> "$code！好评如潮！"
-            in 80..100 -> "$code！不错啦不错啦！"
-            in 60..100 -> "$code！是不错的一天呢！"
-            50 -> "$code！五五开..."
-            in 51..100 -> "$code！还行啦还行啦。"
-            in 40..100 -> "$code！貌似还行？"
-            in 20..100 -> "$code！还……还行吧……？"
-            in 11..100 -> "$code！呜哇……"
-            in 1..100 -> "$code……（没错，是百分制）"
-            else -> "$code..."
+            100 -> "哎呀，博士，您的运气是$code！非同凡响的好运，尽情的享受鲜花，美酒与欢呼吧！"
+            in 80..99 -> "$code，极好的运气呢。我仿佛感受到了高歌与欢呼。享受美好的一天吧。"
+            in 60..80 -> "$code 运气不错。您的路上会有智慧与勇气相伴，放心的前进吧。"
+            in 40..60 -> "您的运气是$code，胜利还需由智慧和努力浇灌，不过也能期待一下偶尔的小幸运呢。"
+            in 20..40 -> "博士，您今天的运气是$code。不必追求荣耀与胜利，普普通通的也不错，不是吗？"
+            in 1..20 -> "您的运气是……$code，不算太好，需要我为您祈求好运吗？"
+            else -> "唔，运气是$code，不是很好呢。不必担心，我已经为您祈求好运了。可以陪我一起逛逛庆典吗？"
         }
     }
 }
